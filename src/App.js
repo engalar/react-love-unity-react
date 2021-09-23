32b5f5eb572d3135caaeeb231b06d55a983fedcb
engalar <engalar@gmail.com>
2021-09-23 09:05:53 +0800

    mendix love unity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const BASE_NAME = 'unity3d/Build/unity3d.';
+
+const unityContext = new UnityContext({
+  loaderUrl: `${BASE_NAME}loader.js`,
+  dataUrl: `${BASE_NAME}data`,
+  frameworkUrl: `${BASE_NAME}framework.js`,
+  codeUrl: `${BASE_NAME}wasm`,
+});
+
+function spawnEnemies() {
+  unityContext.send("Cube", "SpawnEnemies", 100);
+}
 
 function App() {
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [score, setScore] = useState(0);
+
+  useEffect(function () {
+    unityContext.on("GameOver", function (userName, score) {
+      setIsGameOver(true);
+      setUserName(userName);
+      setScore(score);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={spawnEnemies}>Spawn a bunch!</button>
+      {isGameOver === true && <p>{`Game Over! ${userName} ${score} points`}</p>}
+      <Unity unityContext={unityContext} />
     </div>
   );
 }
