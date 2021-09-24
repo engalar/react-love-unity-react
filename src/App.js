ee9c91c6a8f8eb99ea271f113406bddf4e950e4b
engalar <engalar@gmail.com>
2021-09-24 12:16:16 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
+import { Checkbox, Layout, List } from 'antd';
+
+import 'antd/dist/antd.css';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+
 
 const BASE_NAME = 'unity3d/Build/unity3d.';
 
@@ -11,28 +18,41 @@ const unityContext = new UnityContext({
   codeUrl: `${BASE_NAME}wasm`,
 });
 
-function spawnEnemies() {
-  unityContext.send("Cube", "SpawnEnemies", 100);
+function spawnEnemies(e) {
+  console.log(e.target.value, e.target.checked ? "StartShake" : "StopShake");
+  unityContext.send(e.target.value, e.target.checked ? "StartShake" : "StopShake");
 }
 
 function App() {
-  const [isGameOver, setIsGameOver] = useState(false);
   const [userName, setUserName] = useState("");
-  const [score, setScore] = useState(0);
 
   useEffect(function () {
-    unityContext.on("GameOver", function (userName, score) {
-      setIsGameOver(true);
-      setUserName(userName);
-      setScore(score);
+    unityContext.on("ClickObject", function (name, score) {
+      setUserName(name);
     });
   }, []);
 
   return (
     <div>
-      <button onClick={spawnEnemies}>Spawn a bunch!</button>
-      {isGameOver === true && <p>{`Game Over! ${userName} ${score} points`}</p>}
-      <Unity unityContext={unityContext} />
+      <Layout>
+        <Header>Header</Header>
+        <Layout>
+          <Content>
+            <Unity style={{ height: 600, width: 600 }} unityContext={unityContext} />
+          </Content>
+          <Sider>
+            {<p>{`你选择了管道 ${userName}`}</p>}
+            <List>
+              <List.Item><Checkbox value="pipe1" onChange={spawnEnemies}></Checkbox></List.Item>
+              <List.Item><Checkbox value="pipe2" onChange={spawnEnemies}></Checkbox></List.Item>
+              <List.Item><Checkbox value="pipe3" onChange={spawnEnemies}></Checkbox></List.Item>
+              <List.Item><Checkbox value="pipe4" onChange={spawnEnemies}></Checkbox></List.Item>
+              <List.Item><Checkbox value="pipe5" onChange={spawnEnemies}></Checkbox></List.Item>
+            </List>
+          </Sider>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   );
 }
